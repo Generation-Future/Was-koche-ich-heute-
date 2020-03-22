a60813048da30a6ff84fe4d4fb6925fc34c17d3f
Marc Haldenwang <marc.haldenwang@outlook.de>
2020-03-22 14:05:10 +0100

    Initial Commit

---
+++ Was koche ich heute?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Was koche ich heute?
+//
+//  Created by Marc Haldenwang on 22.03.20.
+//  Copyright © 2020 Generation Future. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
